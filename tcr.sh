681edb5e01d586504aabcd87d3b82b09373e2fc1
Joey Chen <jay.hatelove@gmail.com>
2020-01-14 01:35:25 +1100

    working

--- tcr.sh
+++ tcr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while true; do
-  git status -s | grep . || { sleep 1; continue; }
+# while true; do
+#   git status -s | grep . || { sleep 1; continue; }
   npm test && git add . && git commit -am "working" || (git reset --hard; git clean -fd)
-done
+# done
